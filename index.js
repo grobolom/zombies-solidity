cda92b12d75d5390b1d5d5a76511fc0e8389df17
Vasja Volin <vasja.volin@gmail.com>
2018-08-11 23:24:53 -0400

    add right stuff

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static('.'));
 
-app.listen(8000, () => {
-    console.log('Example app listening on port 8000!')
-});
+app.listen(3000, () => console.log('foo'));
